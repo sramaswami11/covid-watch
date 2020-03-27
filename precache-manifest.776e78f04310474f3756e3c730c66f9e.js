ecc4ff6fc57b8db80f5dac1208cff712156d9b5e
Siva Ramaswami <sramaswami11@gmail.com>
2020-03-27 17:45:29 -0400

    Updates

---
+++ precache-manifest.776e78f04310474f3756e3c730c66f9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d0145f30d54246bbab7b60200eac82",
+    "url": "/covid-watch/index.html"
+  },
+  {
+    "revision": "c194056e99ca47ce32de",
+    "url": "/covid-watch/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "018e27acd1a2cfbd0546",
+    "url": "/covid-watch/static/css/main.a9eb63f6.chunk.css"
+  },
+  {
+    "revision": "c194056e99ca47ce32de",
+    "url": "/covid-watch/static/js/2.e64cd970.chunk.js"
+  },
+  {
+    "revision": "b7b068e7a9ff7c79ac98ce106091f1d8",
+    "url": "/covid-watch/static/js/2.e64cd970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "018e27acd1a2cfbd0546",
+    "url": "/covid-watch/static/js/main.cae00b71.chunk.js"
+  },
+  {
+    "revision": "1a9de85200766625296b",
+    "url": "/covid-watch/static/js/runtime-main.c2c87ff2.js"
+  }
+]);
