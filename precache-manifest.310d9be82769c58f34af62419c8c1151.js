9325c58544b33535787d4a8e3f0f325ef6802938
Siva Ramaswami <sramaswami11@gmail.com>
2020-03-29 19:03:23 -0400

    Updates

--- precache-manifest.310d9be82769c58f34af62419c8c1151.js
+++ precache-manifest.310d9be82769c58f34af62419c8c1151.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24ad74ee9d9db6ef818740b66be5f4f1",
+    "revision": "b9305da1eb415fd6cdf7581fb21ec461",
     "url": "/covid-watch/index.html"
   },
   {
-    "revision": "c194056e99ca47ce32de",
+    "revision": "bcdd7b4868f04af902e2",
     "url": "/covid-watch/static/css/2.b90ce945.chunk.css"
   },
   {
-    "revision": "164938a083d5d7c82adb",
+    "revision": "9fde3d768dedd5bee49c",
     "url": "/covid-watch/static/css/main.a9eb63f6.chunk.css"
   },
   {
-    "revision": "c194056e99ca47ce32de",
-    "url": "/covid-watch/static/js/2.e64cd970.chunk.js"
+    "revision": "bcdd7b4868f04af902e2",
+    "url": "/covid-watch/static/js/2.9721499d.chunk.js"
   },
   {
     "revision": "b7b068e7a9ff7c79ac98ce106091f1d8",
-    "url": "/covid-watch/static/js/2.e64cd970.chunk.js.LICENSE.txt"
+    "url": "/covid-watch/static/js/2.9721499d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "164938a083d5d7c82adb",
-    "url": "/covid-watch/static/js/main.1d66b22e.chunk.js"
+    "revision": "9fde3d768dedd5bee49c",
+    "url": "/covid-watch/static/js/main.82cb989d.chunk.js"
   },
   {
     "revision": "1a9de85200766625296b",
